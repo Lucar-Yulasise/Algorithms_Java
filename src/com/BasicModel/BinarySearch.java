0883f1a60a4698d58d7a29051493c457c547e6e3
Lucas <brujah666@outlook.com>
2019-10-20 14:00:42 +0800

    update

--- src/com/BasicModel/BinarySearch.java
+++ src/com/BasicModel/BinarySearch.java
@@ -1,4 +1,6 @@
 package com.BasicModel;
+import edu.princeton.cs.algs4.*;
+
 import java.util.Arrays;
 
 public class BinarySearch {
@@ -6,7 +8,7 @@ public class BinarySearch {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi){
-            int mid  = lo + (hi -lo) /2;
+            int mid  = lo + (hi -lo) / 2;
             if (key <a[mid]) hi = mid -1;
             else if (key > a[mid]) lo = mid + 1;
             else return mid;
@@ -18,9 +20,9 @@ public class BinarySearch {
         int [] whitelist = In.readInts(args[0]); // 这里输入一个整型数据流，填入列表中
         Arrays.sort(whitelist);
         while (!StdIn.isEmpty()){
-            int key = StdInt.readInt();
+            int key = StdIn.readInt();
             if (rank(key,whitelist)== -1){
-                System.out.println(key);
+                StdOut.println(key);
             }
         }
     }
